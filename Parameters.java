a3d1646119c3cefa51167d71b39c18a1ba83fe66
Miriam Striks <mzstriks@gmail.com>
2015-10-29 20:43:33 -0400

    Added likelihood as a measure for convergence and fixed other small bugs.

--- Parameters.java
+++ Parameters.java
@@ -9,22 +9,20 @@
  */
 public class Parameters {
    
-       double pM; // Probability gender is male
-       double pWeightM; // Probability weight > 130 given gender is male
-       double pWeightF; // Probability weight > 130 given gender is female
-       double pHeightM; // Probability height > 55 given gender is male
-       double pHeightF; // Probability height > 55 given gender is female
-
-       double delta = 0.001;
+       double pG0; // Probability gender is male
+       double pG0W0; // Probability weight > 130 given gender is male
+       double pG1W0; // Probability weight > 130 given gender is female
+       double pG0H0; // Probability height > 55 given gender is male
+       double pG1H0; // Probability height > 55 given gender is female
 
        public Parameters(double m, double wm, double wf, 
              double hm, double hf)
        {
-          pM = m;
-          pWeightM = wm;
-          pWeightF = wf;
-          pHeightM = hm;
-          pHeightF = hf;
+          pG0 = m;
+          pG0W0 = wm;
+          pG1W0 = wf;
+          pG0H0 = hm;
+          pG1H0 = hf;
           
        }
        
@@ -33,75 +31,115 @@ public class Parameters {
         * calculates the probability that gender = male,
         * given values for weight and height
         */
-       public double getProbabilityMale(String w, String h){
+       public double getProbabilityMale(int wh){
+          
+          if(wh > 3 || wh < 0)
+             throw new IllegalArgumentException("Argument to getProbabilityMale() must be between 0 and 3");
           
           // case 00
-          if(w.equals("0") && h.equals("0")){
-             return (pWeightM * pHeightM * pM) / 
-                   ((pWeightM * pHeightM * pM) + (pWeightF * pHeightF * (1-pM)));
+          if(wh == 0){
+             return (pG0W0 * pG0H0 * pG0) / 
+                   ((pG0W0 * pG0H0 * pG0) + (pG1W0 * pG1H0 * (1-pG0)));
           }
           // case 01
-          else if(w.equals("0") && h.equals("1")){
-             return (pWeightM * (1-pHeightM) * pM) / 
-                   ((pWeightM * (1-pHeightM) * pM) + (pWeightF * (1-pHeightF) * (1-pM)));
+          else if(wh == 1){
+             return (pG0W0 * (1-pG0H0) * pG0) / 
+                   ((pG0W0 * (1-pG0H0) * pG0) + (pG1W0 * (1-pG1H0) * (1-pG0)));
           }
           //case 10
-          else if(w.equals("1") && h.equals("0")){
-             return ((1-pWeightM) * pHeightM * pM) / 
-                   (((1-pWeightM) * pHeightM * pM) + ((1-pWeightF) * pHeightF * (1-pM)));
+          else if(wh == 2){
+             return ((1-pG0W0) * pG0H0 * pG0) / 
+                   (((1-pG0W0) * pG0H0 * pG0) + ((1-pG1W0) * pG1H0 * (1-pG0)));
           }
           //case 11
-          else return ((1-pWeightM) * (1-pHeightM) * pM) / 
-                (((1-pWeightM) * (1-pHeightM) * pM) + ((1-pWeightF) * (1-pHeightF) * (1-pM)));
+          else return ((1-pG0W0) * (1-pG0H0) * pG0) / 
+                (((1-pG0W0) * (1-pG0H0) * pG0) + ((1-pG1W0) * (1-pG1H0) * (1-pG0)));
           
        }
        
-       /*
-        * Returns true if this parameter is close enough to another parameter
-        * based on the threshold represented by delta; 
-        */      
-       public boolean converged(Parameters other)
-       {
-           return (Math.abs(pM - other.getPM()) < delta &&
-               Math.abs(pWeightM - other.getPWeightM()) < delta &&
-               Math.abs(pWeightF - other.getPWeightF()) < delta &&
-               Math.abs(pHeightM - other.getPHeightM()) < delta &&
-               Math.abs(pHeightF - other.getPHeightF()) < delta);
+      /*
+       * calculates the likelihood of the given complete configuration
+       */
+      public double getLikelihoodComplete(int config){
+         
+         if(config > 7 || config < 0)
+            throw new IllegalArgumentException("Argument to getLikelihoodComplete() must be between 0 and 7");
+         
+         switch(config){
+         //case 000
+         case 0: return pG0W0 * pG0H0 * pG0;
+         //case 001
+         case 1: return pG0W0 * (1-pG0H0) * pG0;
+         //case 010
+         case 2: return (1-pG0W0) * pG0H0 * pG0;
+         //case 011
+         case 3: return (1-pG0W0) * (1-pG0H0) * pG0;
+         //case 100
+         case 4: return pG1W0 * pG1H0 * (1-pG0);
+         //case 101
+         case 5: return pG1W0 * (1-pG1H0) * (1-pG0);
+         //case 110
+         case 6: return (1-pG1W0) * pG1H0 * (1-pG0);
+         //case 111
+         default: return (1-pG1W0) * (1-pG1H0) * (1-pG0);
+               
+         }       
+      }
+      
+      /*
+       * calculates the likelihood of the given incomplete configuration
+       */
+      public double getLikelihoodIncomplete(int config){
+         
+         if(config > 3 || config < 0)
+            throw new IllegalArgumentException("Argument to getLikelihoodComplete() must be between 0 and 3");
+         
+         switch(config){
+         //case 00
+         case 0: return (pG0W0 * pG0H0 * pG0) + (pG1W0 * pG1H0 * (1-pG0));
+         //case 01
+         case 1: return (pG0W0 * (1-pG0H0) * pG0) + (pG1W0 * (1-pG1H0) * (1-pG0));
+         //case 10
+         case 2: return ((1-pG0W0) * pG0H0 * pG0) + ((1-pG1W0) * pG1H0 * (1-pG0));
+         //case 11
+         default: return ((1-pG0W0) * (1-pG0H0) * pG0) + ((1-pG1W0) * (1-pG1H0) * (1-pG0));
+               
+         }       
+      }
+       
 
+       public double getpG0()
+       {
+           return pG0;
        }
 
-       public double getPM()
+       public double getpG0W0()
        {
-           return pM;
-       }
-
-       public double getPWeightM()
-       {
-           return pWeightM;
+           return pG0W0;
        }
        
-       public double getPWeightF()
+       public double getpG1W0()
        {
-           return pWeightF;
+           return pG1W0;
        }
        
-       public double getPHeightM()
+       public double getpG0H0()
        {
-           return pHeightM;
+           return pG0H0;
        }
        
-       public double getPHeightF()
+       public double getpG1H0()
        {
-           return pHeightF;
+           return pG1H0;
        }
 
        public String toString()
        {
-           return String.format("Male: %.3f\n"
-                 + "weight > 130 given male: %.3f\n"
-                 + "weight > 130 given female: %.3f\n"
-                 + "height > 55 given male: %.3f\n"
-                 + "height > 55 given female: %.3f\n", pM, pWeightM, pWeightF, pHeightM, pHeightF);
+           return String.format("X = 0: %.3f\n"
+                 + "X = 0, W = 0: %.3f\n"
+                 + "X = 1, W = 0: %.3f\n"
+                 + "X = 0, H = 0: %.3f\n"
+                 + "X = 1, H = 0: %.3f\n", pG0, pG0W0, pG1W0, pG0H0, pG1H0);
        }
 
 }
